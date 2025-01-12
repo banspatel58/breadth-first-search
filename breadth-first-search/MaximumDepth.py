815d2e6f50498c3a228d178238714405213b6a18
Bansari <banspatel58@gmail.com>
2025-01-12 00:23:42 -0500

    Find Maximum Depth of a Binary Tree

--- breadth-first-search/MaximumDepth.py
+++ breadth-first-search/MaximumDepth.py
@@ -2,6 +2,19 @@ from typing import Optional
 from TreeNode import TreeNode
 from Constructor import TreeConstructor
 
+
+"""
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest 
+path from the root node down to the farthest leaf node.
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Input: root = [1,null,2]
+Output: 2
+"""
 def maximum_depth_binary_tree (root: Optional [TreeNode]) -> int:
     max_depth = 0
 
