f2dce9606b696cb735422276bdc1cdf4e5031479
Bansari <banspatel58@gmail.com>
2025-01-11 23:03:34 -0500

    Handle none's while constructing Binary Tree

--- breadth-first-search/Constructor.py
+++ breadth-first-search/Constructor.py
@@ -7,12 +7,13 @@ class TreeConstructor:
         root = None
 
         if start < end:
-            root = TreeNode(arr [start])
+            if arr[start]:
+                root = TreeNode(arr [start])
 
-            # Insert Nodes on Left Tree
-            root.left = self.insert_level_order(arr, 2 * start + 1, end)
+                # Insert Nodes on Left Tree
+                root.left = self.insert_level_order(arr, 2 * start + 1, end)
 
-            # Insert Nodes on Right Tree
-            root.right = self.insert_level_order(arr, 2 * start + 2, end)
+                # Insert Nodes on Right Tree
+                root.right = self.insert_level_order(arr, 2 * start + 2, end)
 
         return root
